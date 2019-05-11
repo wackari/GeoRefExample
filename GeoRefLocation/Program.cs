448990cc21296442389762df4f9ade8a9c5b9c22
wackari <marckorv87@gmail.com>
2019-05-11 11:26:01 -0400

    Check in Proyect

---
+++ GeoRefLocation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace GeoRefLocation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create("https://nominatim.openstreetmap.org/reverse?format=json&lon=-78.70896&lat=33.81632");
+            objRequest.Method = "GET";
+            objRequest.UserAgent = @"AppName";
+            WebResponse response = (WebResponse)objRequest.GetResponse();
+            HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+            string result = "";
+            using (StreamReader responseStream = new StreamReader(objResponse.GetResponseStream()))
+            {
+                result = responseStream.ReadToEnd();
+                responseStream.Close();
+            }
+
+            var GeoRef = JsonConvert.DeserializeObject<RootObject>(result);
+            Console.WriteLine(GeoRef.display_name);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
